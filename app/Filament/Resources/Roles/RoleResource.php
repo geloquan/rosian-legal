d7f8635a90226f057e8c2cc30cc6580b77faa00d
Marco Angelo Quanico <72260774+geloquan@users.noreply.github.com>
2026-06-02 23:54:04 +0800

    refactor: consolidate RoleResource pages into ManageRoles and streamline table and form configurations

--- app/Filament/Resources/Roles/RoleResource.php
+++ app/Filament/Resources/Roles/RoleResource.php
@@ -2,16 +2,27 @@
 
 namespace App\Filament\Resources\Roles;
 
-use App\Filament\Resources\Roles\Pages\CreateRole;
-use App\Filament\Resources\Roles\Pages\EditRole;
-use App\Filament\Resources\Roles\Pages\ListRoles;
-use App\Filament\Resources\Roles\Schemas\RoleForm;
-use App\Filament\Resources\Roles\Tables\RolesTable;
+use App\Filament\Resources\Roles\Pages\ManageRoles;
 use BackedEnum;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ForceDeleteAction;
+use Filament\Actions\ForceDeleteBulkAction;
+use Filament\Actions\RestoreAction;
+use Filament\Actions\RestoreBulkAction;
+use Filament\Actions\ViewAction;
+use Filament\Forms\Components\Repeater\TableColumn;
+use Filament\Forms\Components\TextInput;
 use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\TrashedFilter;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Spatie\Permission\Models\Role;
 
 class RoleResource extends Resource
@@ -22,27 +33,59 @@ class RoleResource extends Resource
 
   public static function form(Schema $schema): Schema
   {
-    return RoleForm::configure($schema);
+    return $schema
+      ->components([
+        TextInput::make('name')
+          ->required()
+          ->maxLength(255),
+      ]);
+  }
+
+  public static function infolist(Schema $schema): Schema
+  {
+    return $schema
+      ->components([
+        //
+      ]);
   }
 
   public static function table(Table $table): Table
   {
-    return RolesTable::configure($table);
-  }
+    return $table
+      ->columns([
+        TextColumn::make('name')->searchable()->sortable(),
+      ])
+      ->filters([
 
-  public static function getRelations(): array
-  {
-    return [
-      //
-    ];
+      ])
+      ->recordActions([
+        ViewAction::make(),
+        EditAction::make(),
+        DeleteAction::make(),
+        ForceDeleteAction::make(),
+        RestoreAction::make(),
+      ])
+      ->toolbarActions([
+        BulkActionGroup::make([
+          DeleteBulkAction::make(),
+          ForceDeleteBulkAction::make(),
+          RestoreBulkAction::make(),
+        ]),
+      ]);
   }
 
   public static function getPages(): array
   {
     return [
-      'index' => ListRoles::route('/'),
-      'create' => CreateRole::route('/create'),
-      'edit' => EditRole::route('/{record}/edit'),
+      'index' => ManageRoles::route('/'),
     ];
+  }
+
+  public static function getRecordRouteBindingEloquentQuery(): Builder
+  {
+    return parent::getRecordRouteBindingEloquentQuery()
+      ->withoutGlobalScopes([
+        SoftDeletingScope::class,
+      ]);
   }
 }
