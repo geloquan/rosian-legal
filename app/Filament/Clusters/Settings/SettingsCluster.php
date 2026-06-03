f50f32f5a762881091bcc5229611bbd6e1fef3ac
Marco Angelo Quanico <72260774+geloquan@users.noreply.github.com>
2026-06-03 22:20:09 +0800

    feat: add subNavigationPosition to SettingsCluster for improved navigation layout

--- app/Filament/Clusters/Settings/SettingsCluster.php
+++ app/Filament/Clusters/Settings/SettingsCluster.php
@@ -4,9 +4,12 @@ namespace App\Filament\Clusters\Settings;
 
 use BackedEnum;
 use Filament\Clusters\Cluster;
+use Filament\Pages\Enums\SubNavigationPosition;
 use Filament\Support\Icons\Heroicon;
 
 class SettingsCluster extends Cluster
 {
   protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedSquares2x2;
+  protected static ?SubNavigationPosition $subNavigationPosition = SubNavigationPosition::Top;
+
 }
