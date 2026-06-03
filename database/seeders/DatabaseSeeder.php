51a6c13480c48f3cc5a6a0f52c67c0c315eafaa1
Marco Angelo Quanico <72260774+geloquan@users.noreply.github.com>
2026-06-03 12:33:06 +0800

    feat: add cluster discovery to AdminPanelProvider

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,21 +5,23 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
+  use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void
+  {
+    $user = User::factory()->create([
+      'name' => 'Marco',
+      'email' => 'marcoangelo.quanico@gmail.com',
+      'password' => Hash::make('1234'),
+    ]);
+    $role = \Spatie\Permission\Models\Role::create(['name' => 'super-admin']);
+    $user->assignRole($role);
+  }
 }
