458272f574c761b055c2b30269efe62ca6eb6615
Marco Angelo Quanico <72260774+geloquan@users.noreply.github.com>
2026-06-03 12:41:47 +0800

    feat: add PrinterConnection model and migration for managing printer connections

--- app/Models/PrinterConnection.php
+++ app/Models/PrinterConnection.php
@@ -9,7 +9,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 #[Fillable(['name', 'ip_address', 'port'])]
 #[Hidden(['created_at', 'updated_at'])]
-#[]
 class PrinterConnection extends Model
 {
   use SoftDeletes;
