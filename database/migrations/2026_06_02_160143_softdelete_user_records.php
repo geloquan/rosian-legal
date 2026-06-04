8de207d31b25c272dd8c1c06b40163d014ed8ba0
Marco Angelo Quanico <72260774+geloquan@users.noreply.github.com>
2026-06-04 23:35:56 +0800

    feat: implement soft delete rollback for user records

--- database/migrations/2026_06_02_160143_softdelete_user_records.php
+++ database/migrations/2026_06_02_160143_softdelete_user_records.php
@@ -20,6 +20,8 @@ return new class extends Migration {
    */
   public function down(): void
   {
-    //
+    Schema::table('users', function (Blueprint $table) {
+      $table->dropSoftDeletes();
+    });
   }
 };
